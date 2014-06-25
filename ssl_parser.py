8351a477aad61278358f064149a5b5e36a3f2051
lthurlow <lincoln@dhcp-2-163.ucsc.edu>
2014-06-25 12:58:40 -0700

    few more modifications before done

--- ssl_parser.py
+++ ssl_parser.py
@@ -49,39 +49,94 @@ csv_write.writerow(["IP","Hostname", "Alg", "Bits", "Cert Name","Org Name",\
 
 if ("csv" in usr_input):
   for line in nmap_file:
-    split_line = re.split(',|\||\n',line)
-    if len(split_line) > 14:
-      pdb.set_trace()
-      IP =  split_line[0]
-      LOGICALSEG = ' '.join(split_line[1].split(" ")[0:2])
-      TYPE = split_line[8].split(':')[1]
-      BITS = split_line[9].split(':')[1]
-      COMNAME = split_line[2].split('=')[1]
-      ORGNAME = split_line[3].split('=')[1]
-      STATE = split_line[6].split('=')[1]
-      COUNTRY = split_line[7].split('=')[1]
-      BEFORE = datetime.datetime.fromtimestamp(time.mktime(time.strptime\
-               (split_line[13].strip(),"%d %b %Y %H:%M:%S %Z")))\
-                .strftime("%m/%d %Y").lstrip('0')
-      AFTER = datetime.datetime.fromtimestamp(time.mktime(time.strptime\
-               (split_line[11].strip(),"%d %b %Y %H:%M:%S %Z")))\
-                .strftime("%m/%d %Y").lstrip('0')
-      ISSUER = split_line[2].split('=')[1]
-      SELFSIGN = split_line[14].split(':')[1]
-      VALIDSIG = split_line[-2].split(':')[1][:-1]
-      print IP
-      print LOGICALSEG
-      print TYPE
-      print BITS
-      print COMNAME
-      print ORGNAME
-      print STATE
-      print COUNTRY
-      print BEFORE
-      print AFTER
-      print ISSUER
-      print SELFSIGN
-      print VALIDSIG
+    #split_line = re.split(',|\||\n',line)
+    split_line = re.split('\n',line)
+    for part_line in split_line:
+      if len(part_line.split(',')) < 10:
+        continue
+      IP = part_line.split(',')[0]
+      LOGICALSEG = ' '.join(part_line.split(',')[1].split(" ")[0:2])
+
+      EXTRA = []
+
+      RE_TYPE = re.compile(r'ssl\.cert\.key\.alg\.name\:(.*?)\|')
+      RE_BITS = re.compile(r'ssl\.cert\.key\.rsa\.modulusBits\:(.+?)\|') 
+      RE_AFTER = re.compile(r'ssl\.cert\.not\.valid\.after\:\w+, (.+?)\|')
+      RE_BEFORE = re.compile(r'ssl\.cert\.not\.valid\.before\:\w+, (.+?)\|')
+      RE_ISSUER = re.compile(r'ssl\.cert\.issuer\.dn\:(.+?)\|')
+      RE_ERROR = re.compile(r'ssl\.cert\.chainerror\:(.+?)\|')
+      RE_SELFSIGN = re.compile(r'ssl\.cert\.selfsigned\:(.+?)\|')
+      RE_VALIDSIGN = re.compile(r'ssl\.cert\.validchain\:(.+?)\|')
+      
+      TYPE = ''.join(RE_TYPE.findall(part_line)).strip()
+      BITS = ''.join(RE_BITS.findall(part_line)).strip()
+      ERROR = ''.join(RE_ERROR.findall(part_line))
+      SELFSIGN = ''.join(RE_SELFSIGN.findall(part_line))
+      VALIDSIGN = ''.join(RE_VALIDSIGN.findall(part_line))
+
+      A_ISSUER = ''.join(RE_ISSUER.findall(part_line))
+      ISSUER = ""
+      ORGNAME = ""
+      ORGUNIT = ""
+      STATE = ""
+      COUNTRY = ""
+      CITY=""
+      EMAIL =""
+      for a in A_ISSUER.split(','):
+        if "CN=" in a:
+          ISSUER = a.split('CN=')[1]
+        elif "OU=" in a:
+          ORGUNIT = a.split('OU=')[1]
+        elif "O=" in a:
+          ORGNAME = a.split('O=')[1]
+        elif "L=" in a:
+          CITY = a.split('L=')[1]
+        elif "ST=" in a:
+          STATE = a.split('ST=')[1]
+        elif "C=" in a:
+          COUNTRY = a.split('C=')[1] 
+        elif "EMAILADDRESS=" in a:
+          EMAIL = a.split('EMAILADDRESS=')[1]
+
+
+
+      if (len(RE_AFTER.findall(part_line)) == 1 ):
+        AFTER_RAW = ''.join(RE_AFTER.findall(part_line)).strip()
+        AFTER = datetime.datetime.fromtimestamp(time.mktime(time.strptime\
+                 (AFTER_RAW.strip(),"%d %b %Y %H:%M:%S %Z")))\
+                  .strftime("%m/%d %Y").lstrip('0')
+        BEFORE_RAW = ''.join(RE_BEFORE.findall(part_line)).strip()
+        BEFORE = datetime.datetime.fromtimestamp(time.mktime(time.strptime\
+                 (BEFORE_RAW.strip(),"%d %b %Y %H:%M:%S %Z")))\
+                  .strftime("%m/%d %Y").lstrip('0')
+       
+      else:
+        IP = "*** " + IP + " ***" 
+        EXTRA = []
+        for x in xrange(0, len(RE_AFTER.findall(part_line))):
+          AFTER_RAW = RE_AFTER.findall(part_line)
+          AFTER = datetime.datetime.fromtimestamp(time.mktime(time.strptime\
+                   (AFTER_RAW[x].strip(),"%d %b %Y %H:%M:%S %Z")))\
+                    .strftime("%m/%d %Y").lstrip('0')
+          BEFORE_RAW = RE_BEFORE.findall(part_line)
+          BEFORE = datetime.datetime.fromtimestamp(time.mktime(time.strptime\
+                   (BEFORE_RAW[x].strip(),"%d %b %Y %H:%M:%S %Z")))\
+                    .strftime("%m/%d %Y").lstrip('0')
+          EXTRA += [BEFORE, AFTER]
+
+
+      #    ## create list of list with order types indexed by for loop
+        """
+        print ''
+        print IP+', '+LOGICALSEG
+        print ERROR
+        print TYPE+', '+ BITS
+        print BEFORE+', '+ AFTER
+        print ISSUER+', '+ ORGNAME+', '+ ORGUNIT
+        print 'Loc: '+CITY+', '+ STATE+', '+COUNTRY
+        print 'Sig: '+SELFSIGN+', '+VALIDSIGN
+        print ''
+        """
 
 if ("nmap" in usr_input):
   each_host = []
